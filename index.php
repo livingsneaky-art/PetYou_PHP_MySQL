e5829a0048fd6e7659acfb754e39d3d5b9c3fa1b
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2022-10-01 12:33:54 +0800

    sql
    For type deliver, enum admin and customer, payment details, delivery details, product cars and its designated foreign key and primary key with constraints

--- index.php
+++ index.php
@@ -13,10 +13,18 @@
             lName VARCHAR(100) NOT NULL,
             uName VARCHAR(100) NOT NULL,
             password VARCHAR(100) NOT NULL,
+            user_type enum('Admin', 'Customer'),
             CONSTRAINT pk_user PRIMARY KEY(id)
         )");
 
-        
+        $conn->query("CREATE TABLE type_delivery(    
+            id INT NOT NULL AUTO_INCREMENT,
+            title VARCHAR(100) NOT NULL,
+            image_name VARCHAR(255) NOT NULL,
+            CONSTRAINT pk_type_delivery PRIMARY KEY(id)
+        )");
+
+
         $conn->query("CREATE TABLE product(
              id INT NOT NULL AUTO_INCREMENT,
             title VARCHAR(100) NOT NULL,
@@ -25,12 +33,13 @@
             image VARCHAR(255) NOT NULL,
             CONSTRAINT pk_product PRIMARY KEY(id)
         )");
+
+        
        
 
         $conn->query("CREATE TABLE payment_details(
              id INT NOT NULL AUTO_INCREMENT,
-            extras_total DECIMAL(11, 2),
-            menus_total DECIMAL(11, 2),
+            products_total DECIMAL(11, 2),
             total DECIMAL(11, 2) NOT NULL,
             minPayment DECIMAL(11, 2) NOT NULL,
             paid DECIMAL(11, 2) NOT NULL,
@@ -53,15 +62,23 @@
             CONSTRAINT pk_bookings PRIMARY KEY(id)
         )");
 
-        $conn->query("CREATE TABLE event_details(
+        $conn->query("CREATE TABLE product_carts(
+            type INT NOT NULL,
+            cartID INT NOT NULL,
+            quantity INT NOT NULL,
+            CONSTRAINT fk_cart_product FOREIGN KEY(cartID) REFERENCES cart(id),
+            CONSTRAINT fk_type_product FOREIGN KEY(type) REFERENCES product(id)
+        )");                
+
+        $conn->query("CREATE TABLE delivery_details(
             id INT NOT NULL,
-            eventID INT,
+            deliveryID INT,
             startTime VARCHAR(200) NOT NULL,
             endTime VARCHAR(200) NOT NULL,
-            eventAddress VARCHAR(200) NOT NULL,
-            event_type INT NOT NULL,
-            CONSTRAINT fk_event_details FOREIGN KEY(event_type) REFERENCES events(id),
-            CONSTRAINT pk_event_details PRIMARY KEY(id)
+            deliveryAddress VARCHAR(200) NOT NULL,
+            delivery_type INT NOT NULL,
+            CONSTRAINT fk_delivery_details FOREIGN KEY(delivery_type) REFERENCES type_delivery(id),
+            CONSTRAINT pk_delivery_details PRIMARY KEY(id)
         )");
 
     }            
